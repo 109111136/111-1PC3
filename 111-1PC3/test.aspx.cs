60bc07c59157eeb5a107c4cea8a7ef0e07658f40
109111136 <h226810797@gmail.com>
2022-09-21 13:19:47 +0800

    hi

---
+++ 111-1PC3/test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC3
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Clothes = 7;
+            int i_Hat = 8;
+            int i_Pants = 9;
+            int price = 0;
+            price = (300 * i_Clothes) + (350 * i_Hat) + (400 * i_Pants);
+            Response.Write(price);
+        }
+    }
+}
